4bbbc599a3d1ec2e177e573da7e0612840877d49
Carlos H.C.AL <98360843+Carlos-hcal@users.noreply.github.com>
2024-02-24 20:31:48 -0300

    feat start page

---
+++ src/pages/Start/start.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import FirstSection from './FirstSection/firstSection'
+import SecondSection from './SecondSection/secondSection'
+import ThirdSection from './ThirdSection/thirdSection'
+
+function Start() {
+  return (
+    <>
+      <FirstSection/>
+      <SecondSection/>
+      <ThirdSection/> 
+    </>
+  )
+}
+
+export default Start
