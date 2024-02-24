a9915749a1d9dcb55dfb221251fcaea449b255c7
Carlos H.C.AL <98360843+Carlos-hcal@users.noreply.github.com>
2024-02-24 20:28:53 -0300

    feat header

---
+++ src/components/Header/Components/header.tsx
@@ -0,0 +1,19 @@
+import { Link } from "react-router-dom";
+import Logo from "./Logo/logo";
+import "./header.css"
+
+const Header = () => {
+    return (
+        <nav className="App-header"> 
+            <Logo />
+            <ul className='pages'>
+                <li className='links'><Link to="/inicio" className="link">INICIO</Link></li>
+                <li className='links'><Link to="/player" className="link">PLAYER</Link></li>
+                <li className='links'><Link to="/projetos" className="link">PROJETOS</Link></li>
+                <li className='links'><Link to="/contato" className="link">CONTATO</Link></li>
+            </ul>
+        </nav>
+    )
+};
+
+export default Header;
