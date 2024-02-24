f3d8a9c3ac1c1157415a372964ae95ba7dca3222
Carlos H.C.AL <98360843+Carlos-hcal@users.noreply.github.com>
2024-02-24 20:29:08 -0300

    feat Logo

---
+++ src/components/Header/Components/Logo/logo.tsx
@@ -0,0 +1,12 @@
+import { ReactComponent as LogoSvg } from './src/Logo.svg'
+import "./logo.css"
+
+function Logo() {
+    return (
+        <div className="imglogo">
+            <LogoSvg />
+        </div>
+    )
+}
+
+export default Logo
