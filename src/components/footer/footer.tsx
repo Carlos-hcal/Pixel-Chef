d04935449e1fc82b0f85b521db390399530e7725
Carlos H.C.AL <98360843+Carlos-hcal@users.noreply.github.com>
2024-02-24 20:28:35 -0300

    feat Footer

---
+++ src/components/footer/footer.tsx
@@ -0,0 +1,42 @@
+import "./footer.css"
+import Logo from "../Header/Components/Logo/logo"
+import Linkedin from './src/Linkedin.svg'
+import Github from './src/Github.svg'
+import Whatsapp from './src/Whatsapp.svg'
+import { Link } from "react-router-dom";
+
+function Footer() {
+    const icones = [Linkedin, Github, Whatsapp]
+
+    return (
+      <footer className='App-footer'>
+        <Logo />
+        <ul className='PaginasFooter'>
+          <li className='links-footer'>
+              <Link to={`/`} className="link-footer">INICIO</Link>
+          </li>
+          <li className='links-footer'>
+              <Link to={`/player`} className="link-footer">PLAYER</Link>
+          </li>
+          <li className='links-footer'>
+              <Link to={`/projects`} className="link-footer">PROJETOS</Link>
+          </li>
+          <li className='links-footer'>
+              <Link to={`/contact`} className="link-footer">CONTATO</Link>
+          </li>
+        </ul>
+        <ul className='icones'>
+          {icones.map( (icone) => (
+            <li>
+              <img src={icone}></img>
+            </li>
+          ) ) }
+        </ul>
+        <small>© 2023 Pixel Chef.</small>
+      </footer>
+    ); 
+};
+
+export default Footer
+
+
