1af7dfe7c074625021e80be88907a9e37de8e55c
Carlos H.C.AL <98360843+Carlos-hcal@users.noreply.github.com>
2024-02-25 02:08:37 -0300

    fix bugs

--- src/pages/Projects/projects.tsx
+++ src/pages/Projects/projects.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import "./projects.css"
 import InputSearch from './Search/inputSearch'
+import DefaultCards from './DefaultCards/defaultCard'
 
 function Projects() {
   return (
     <section className='body-projects'>
       <h1 className='title'>Projetos:</h1>
       <InputSearch />
+      <DefaultCards />
     </section>
   )
 }
