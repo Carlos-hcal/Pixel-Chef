a8f70a0ecce7c54d6e4166e9d731c6fe48766ec7
Carlos H.C.AL <98360843+Carlos-hcal@users.noreply.github.com>
2024-02-24 21:30:04 -0300

    feat routes pages

---
+++ src/pages/Contact/contact.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Contact() {
+  return (
+    <div>contact</div>
+  )
+}
+
+export default Contact
---
+++ src/pages/NotFound/notFound.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Notfound() {
+  return (
+    <div>Notfound</div>
+  )
+}
+
+export default Notfound
