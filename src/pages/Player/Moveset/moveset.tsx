d2d75fd496b4eda99d4c4423e198804b94e19a50
Carlos H.C.AL <98360843+Carlos-hcal@users.noreply.github.com>
2024-02-24 23:38:28 -0300

    feat third section player page

--- src/pages/Player/Moveset/moveset.tsx
+++ src/pages/Player/Moveset/moveset.tsx
@@ -1,4 +1,12 @@
 import React from 'react'
+import { FaReact } from "react-icons/fa";
+import { TbBrandTypescript } from "react-icons/tb";
+import { FaSass } from "react-icons/fa";
+import { GiElephant } from "react-icons/gi";
+import { FaJava } from "react-icons/fa";
+import { TbBrandNextjs } from "react-icons/tb";
+import { SiAngularjs } from "react-icons/si";
+import { SiCsharp } from "react-icons/si";
 import "./moveset.css"
 
 function Moveset() {
@@ -6,17 +14,17 @@ function Moveset() {
     <section className='moveset-box'>
         <ul className='moveset'>
             <li className='badge' id="move" >Moveset Atual</li>
-            <li className='badge' id="react">React</li>
-            <li className='badge' id="ts">TypeScript</li>
-            <li className='badge' id="sass">Sass</li>
-            <li className='badge' id="php">PHP</li>
+            <li className='badge' id="react">React <span className='icon'><FaReact /></span></li>
+            <li className='badge' id="ts">TypeScript <span className='icon'><TbBrandTypescript /></span></li>
+            <li className='badge' id="sass">Sass <span className='icon'><FaSass /></span></li>
+            <li className='badge' id="php">PHP <span className='icon'><GiElephant /></span></li>
         </ul>
         <ul className='moveset'>
             <li className='badge' id="move">Moveset Futuro</li>
-            <li className='badge' id="java">Java</li>
-            <li className='badge' id="next">Next.js</li>
-            <li className='badge' id="angular">Angular</li>
-            <li className='badge' id="C">C#</li>
+            <li className='badge' id="java">Java <span className='icon'><FaJava /></span></li>
+            <li className='badge' id="next">Next.js <span className='icon'><TbBrandNextjs /></span></li>
+            <li className='badge' id="angular">Angular <span className='icon'><SiAngularjs /></span></li>
+            <li className='badge' id="C">C# <span className='icon'><SiCsharp /></span></li>
         </ul>
     </section>
   )
