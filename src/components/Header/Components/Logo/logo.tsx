6a2ffd367faf19f6d6c45e95f31a29a1a160d56c
Carlos H.C.AL <carlos.hcal@gmail.com>
2024-02-28 14:23:06 -0300

    fix bugs logo

--- src/components/Header/Components/Logo/logo.tsx
+++ src/components/Header/Components/Logo/logo.tsx
@@ -1,10 +1,11 @@
 import { ReactComponent as LogoSvg } from './src/Logo.svg'
 import "./logo.css"
+import { Link } from 'react-router-dom'
 
 function Logo() {
     return (
         <div className="imglogo">
-            <LogoSvg />
+            <Link to="/inicio" ><LogoSvg /></Link>
         </div>
     )
 }
