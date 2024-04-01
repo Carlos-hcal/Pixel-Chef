c6bcd3c625cf6819ba8b4bee142ff79e9004b3c6
Carlos H.C.A.L <carlos.hcal@gmail.com>
2024-04-01 14:37:57 -0300

    Refact start page

--- src/pages/Start/start.tsx
+++ src/pages/Start/start.tsx
@@ -2,14 +2,17 @@ import React from 'react'
 import FirstSection from './FirstSection/firstSection'
 import SecondSection from './SecondSection/secondSection'
 import ThirdSection from './ThirdSection/thirdSection'
+import './start.css'
+import Dashline from "./src/Dash_line.png"
 
 function Start() {
   return (
-    <>
+    <div className="body-start">
+      <img className='dashline' src={Dashline} alt="linha" />
       <FirstSection/>
       <SecondSection/>
       <ThirdSection/> 
-    </>
+    </div>
   )
 }
 
